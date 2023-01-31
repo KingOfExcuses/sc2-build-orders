6acc578adf7c798ba00c49d0f84ff16d37efb416
KingOfExcuses <kingofexcusestv@gmail.com>
2023-01-31 15:59:37 -0600

    Added filtering by style

--- src/components/styleFilter.tsx
+++ src/components/styleFilter.tsx
@@ -1,21 +1,21 @@
 import React from 'react'
 
-export const ecoBuildType = 'Economic'
-export const timingAttackBuildType = 'Timing Attack'
 export const allInBuildType = 'All In'
 export const cheeseBuildType = 'Cheese'
+export const ecoBuildType = 'Economic'
+export const timingAttackBuildType = 'Timing Attack'
 export const buildTypes = [
-  ecoBuildType,
-  timingAttackBuildType,
   allInBuildType,
   cheeseBuildType,
+  ecoBuildType,
+  timingAttackBuildType,
 ]
 
 const StyleFilter = () => {
   return (
-    <>
-      <h2 className="mb-4 font-semibold">Style</h2>
-      <ul className="w-1/6 items-center rounded-lg bg-navy text-sm font-medium">
+    <div className="w-1/6">
+      <h1 className="mb-2 font-semibold">Style</h1>
+      <ul className="items-center rounded-lg bg-navy text-sm font-medium">
         {buildTypes.map((buildType) => (
           <li key={buildType} className="w-full border-b border-gray-light">
             <div className="flex items-center pl-3">
@@ -36,7 +36,7 @@ const StyleFilter = () => {
           </li>
         ))}
       </ul>
-    </>
+    </div>
   )
 }
 
